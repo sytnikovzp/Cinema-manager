7e66552fc0bbbc63c2ce16576a502e21d88a1269
alex <sytnikov.zp@gmail.com>
2024-09-05 15:04:20 +0300

    Disabled 'strict mode' on client

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 // =============================
@@ -7,9 +6,7 @@ import store from './store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
